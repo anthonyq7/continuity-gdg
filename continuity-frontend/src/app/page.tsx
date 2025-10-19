b6eee4c6be1dfeefec189f84bc3e40ca46d0a32d
Michelle Lin <michellelin@Michelles-MacBook-Pro.local>
2025-10-19 14:00:02 -0500

    Added new tsx pages

--- continuity-frontend/src/app/page.tsx
+++ continuity-frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import login_pic from "@/assets/login_pic.png";
 
 export default function Home() {
   return (
-    <div style={{ backgroundColor: "#000000" }}>
+    <div style={{ backgroundColor: "#000000"}}>
       <Image src={login_pic} alt="login" />;
       <h1 style={{ position: "absolute", left: "1110px", top: "250px", color: "#FFFFFF", fontSize: "48px", fontWeight: "bold", }}>Welcome Back!</h1>
       <p style={{ position: "absolute", left: "1110px", top: "320px", color: "#FFFFFF", fontSize: "30px" }}>log in 🎉</p>
